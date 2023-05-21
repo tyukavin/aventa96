f05d869a682626705b4727a35b34f752ef10b321
Denis Tyukavin <dtyukavin@dmvdesk.com>
2023-05-22 01:15:40 +0300

    add ftp settings

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -8,7 +8,7 @@ const connect = ftp.create(ftpSettings);
 
 module.exports = function deploy() {
 	return src(['build/**/*.*', '!build/**/*.map'])
-		.pipe(connect.newer('public_html/'))
-		.pipe(connect.dest('public_html/'))
+		.pipe(connect.newer('www/aventa96.dtyukavin.ru'))
+		.pipe(connect.dest('www/aventa96.dtyukavin.ru'))
 		.on('end', () => console.log(`Finished deploing ./build to https://${chalk.blueBright(ftpSettings.host)}`))
 }
