38ba68aacdbc19ce7f924da4bec69c334bd1d11d
Denis Tyukavin <dtyukavin@dmvdesk.com>
2023-08-07 10:37:46 +0300

    pages init

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -95,4 +95,39 @@ $(document).ready(function() {
         $(".js-card-pic img").removeClass("active");
         $(".js-card-pic img").eq($(this).index()).addClass("active");
     });
+
+    $(".js-card-btn-tab").click(function() {
+
+        $(".js-card-btn-tab").removeClass("active");
+        $(this).addClass("active");
+        $(".js-card-tab").removeClass("active");
+        $(".js-card-tab").eq($(this).index()).addClass("active");
+    });
+
+    $(".js-user-cart-tab").click(function() {
+
+        $(".js-user-cart-tab").removeClass("active");
+        $(this).addClass("active");
+        $(".js-user-cart-content").removeClass("active");
+        $(".js-user-cart-content").eq($(this).index()).addClass("active");
+    });
+
+    $(".js-delivery-cart-tab").click(function() {
+
+        $(".js-delivery-cart-tab").removeClass("active");
+        $(this).addClass("active");
+        $(".js-delivery-cart-content").removeClass("active");
+        $(".js-delivery-cart-content").eq($(this).index()).addClass("active");
+    });
+
+    $(".js-cart-pay").click(function() {
+
+        $(".js-cart-pay").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".overview-toggle-btn").click(function() {
+
+        $(this).parent().toggleClass("active");
+    });
 });
